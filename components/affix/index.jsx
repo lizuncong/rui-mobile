80f19c7fd82d9db73c153946e14e439fb43c53f8
lizuncong <2900170896@qq.com>
2020-09-28 19:45:33 +0800

    stash

---
+++ components/affix/index.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Index = memo(() => {
+    return (
+        <div>
+            这是一个affix组件....
+        </div>
+    )
+})
+
+export default Index
--- components/index.js
+++ components/index.js
@@ -1,1 +1,19 @@
-console.log('haha');
+/* @remove-on-es-build-begin */
+// 如果使用了https://github.com/ant-design/babel-plugin-import，则这个index.js文件就没有作用了。
+const ENV = process.env.NODE_ENV;
+if (
+    ENV !== 'production' &&
+    ENV !== 'test' &&
+    typeof console !== 'undefined' &&
+    console.warn && // eslint-disable-line no-console
+    typeof window !== 'undefined'
+) {
+    // eslint-disable-next-line no-console
+    console.warn(
+        '你正在使用rui-mobile的整个包，' +
+        '请使用https://www.npmjs.com/package/babel-plugin-import做按需加载，以减少包的大小',
+    );
+}
+/* @remove-on-es-build-end */
+
+export { default as Affix } from './affix';
